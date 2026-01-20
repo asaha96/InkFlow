d6cc590911d7674b25c7e9fff67b877fe0de2535
aritrasaha1 <asaha7@gsu.student.edu>
2026-01-19 22:37:11 -0500

    fix: Update platform requirements for SwiftSoup compatibility

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ManhwaReader",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(name: "ManhwaReader", targets: ["ManhwaReader"])
     ],
